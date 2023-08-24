4167cd8ca7aa183bd121d5814d11585c95e4cf76
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-08-24 13:05:51 -0500

    Validar asunto y mensaje

--- validacion.js
+++ validacion.js
@@ -1,52 +1,60 @@
-const inputs = document.querySelectorAll("input");
+(() => {
+  const inputs = document.querySelectorAll("input");
+  const textarea = document.querySelector("textarea");
 
-inputs.forEach( (input) => {
+  inputs.forEach((input) => {
     input.addEventListener("blur", (input) => {
-        valida(input.target);
+      valida(input.target);
     });
-});
+  });
+  textarea.addEventListener("blur", (input) => {
+    valida(input.target);
+  });
 
-function valida(input) {
+  function valida(input) {
     const tipoDeInput = input.dataset.tipo;
 
-    if(input.validity.valid) {
-        input.parentElement.classList.remove("input-container--invalid");
-        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    if (input.validity.valid) {
+      input.parentElement.classList.remove("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML = "";
     } else {
-        input.parentElement.classList.add("input-container--invalid");
-        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+      input.parentElement.classList.add("input-container--invalid");
+      input.parentElement.querySelector(".input-message-error").innerHTML =
+        mostrarMensajeDeError(tipoDeInput, input);
     }
-}
+  }
 
-
-const tipoDeErrores = [
+  const tipoDeErrores = [
     "valueMissing",
     "typeMismatch",
     "patternMismatch",
     "customError",
-]
+  ];
 
-const mensajesDeError = {
+  const mensajesDeError = {
     nombre: {
-        valueMissing: "El campo nombre no puede estar vacio"
+      valueMissing: "El campo nombre no puede estar vacio",
     },
     email: {
-        valueMissing: "El campo email no puede estar vacio",
-        typeMismatch: "El correo no es valido (ej: nombre@empresa.com)"
+      valueMissing: "El campo email no puede estar vacio",
+      typeMismatch: "El correo no es valido (ej: nombre@empresa.com)",
     },
-    numero: {
-        valueMissing: "El campo telefono no puede estar vacio",
-        patternMismatch: "El formato requerido es XXXXXXXXXX 10 numeros"
+    asunto: {
+      valueMissing: "El campo asunto no puede estar vacio",
     },
-}
+    mensaje: {
+      valueMissing: "El campo mensaje no puede estar vacio",
+    },
+  };
 
-function mostrarMensajeDeError(tipoDeInput, input) {
+  function mostrarMensajeDeError(tipoDeInput, input) {
     let mensaje = "";
-    tipoDeErrores.forEach( (error) => {
-        if(input.validity[error]) {
-            mensaje = mensajesDeError[tipoDeInput][error];
-        }
-    })
+    tipoDeErrores.forEach((error) => {
+      if (input.validity[error]) {
+        mensaje = mensajesDeError[tipoDeInput][error];
+      }
+    });
 
-    return mensaje
-}
+    return mensaje;
+  }
+})();
