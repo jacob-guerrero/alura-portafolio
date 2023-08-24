4b562ffe45dc55a5dd6aa01d8b8db8b6d8f28ee1
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2023-08-24 12:13:20 -0500

    Agregar validacion con JS en nombre

--- validacion.js
+++ validacion.js
@@ -1,1 +1,52 @@
-//Haz tú validación en javascript acá
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach( (input) => {
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    });
+});
+
+function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
+}
+
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio"
+    },
+    email: {
+        valueMissing: "El campo email no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    numero: {
+        valueMissing: "El campo telefono no puede estar vacio",
+        patternMismatch: "El formato requerido es XXXXXXXXXX 10 numeros"
+    },
+}
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach( (error) => {
+        if(input.validity[error]) {
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    })
+
+    return mensaje
+}
